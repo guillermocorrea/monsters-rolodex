f7ed2c6d98779b343652e718f5c6573aa1677bdd
Guillermo Correa <guillermo.correa.99@gmail.com>
2019-12-12 22:20:06 -0500

    Updates

---
+++ precache-manifest.7cc8cbd0c7737fcabe654ac95f15e705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7128332ea9c1cc63a04bd52517518f0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2493abd4566a5a704b88",
+    "url": "/monsters-rolodex/static/css/main.10ab6184.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2493abd4566a5a704b88",
+    "url": "/monsters-rolodex/static/js/main.8a884436.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
